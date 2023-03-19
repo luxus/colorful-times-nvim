7ff2459a4ba9b12682ffe6af32fb012f4a774703
luxus <luxuspur@gmail.com>
2023-03-19 17:36:57 +0100

    some performance update

--- lua/colorful-times-nvim/theme.lua
+++ lua/colorful-times-nvim/theme.lua
@@ -5,7 +5,7 @@ local theme = {}
 -- function.
 function theme.set(theme_name, bg_name)
 	vim.cmd("colorscheme " .. theme_name)
-	vim.o.background = bg_name
+	vim.api.nvim_set_option("background", bg_name)
 end
 
 function theme.current(frames, opts, current_minutes)
@@ -32,12 +32,13 @@ end
 -- Returns:
 -- A table containing the theme name and background name of the next theme,
 -- and the number of minutes until the next theme starts.
+
 function theme.next(frames, opts, current_minutes)
-	local next_index = nil
 	local next_delay = nil
 	local theme_name, bg_name
+	local i
 
-	for i, v in ipairs(frames) do
+	for idx, v in ipairs(frames) do
 		local start_minutes = v[1]
 		local stop_minutes = v[2]
 		theme_name = v[3]
@@ -45,20 +46,19 @@ function theme.next(frames, opts, current_minutes)
 
 		if current_minutes >= start_minutes and current_minutes < stop_minutes then
 			-- Current timeframe is active
-			next_index = i
 			next_delay = stop_minutes - current_minutes
+			i = idx
 			break
 		elseif current_minutes < start_minutes then
 			-- Next timeframe is about to start
-			next_index = i
 			next_delay = start_minutes - current_minutes
+			i = idx
 			break
 		end
 	end
 
-	if next_index == nil then
+	if next_delay == nil then
 		-- Wrap around to the beginning of the schedule
-		next_index = 1
 		next_delay = frames[1][1] - current_minutes
 		if next_delay < 0 then
 			next_delay = next_delay + 24 * 60
@@ -68,29 +68,15 @@ function theme.next(frames, opts, current_minutes)
 		theme_name = opts.default.theme
 		bg_name = opts.default.bg
 	else
-		-- Adjust current timeframe to end at stop time
-		local current_frame = frames[next_index]
-		if current_minutes >= current_frame[2] then
-			-- Current timeframe has already ended, move to next
-			next_index = next_index + 1
-			if next_index > #frames then
-				-- Wrap around to the beginning of the schedule
-				next_index = 1
-			end
-			current_frame = frames[next_index]
-		end
-		local adjusted_stop = current_frame[2]
-		next_delay = adjusted_stop - current_minutes
-
 		-- Get details of next theme
-		theme_name = current_frame[3]
-		bg_name = current_frame[4] or opts.default.bg
+		theme_name = frames[i][3]
+		bg_name = frames[i][4] or opts.default.bg
 
 		-- Adjust start time of next timeframe
-		if next_delay == 0 and next_index < #frames then
-			local next_frame = frames[next_index + 1]
+		if next_delay == 0 and i < #frames then
+			local next_frame = frames[i + 1]
 			local adjusted_start = next_frame[1]
-			next_delay = (adjusted_start - adjusted_stop) % (24 * 60)
+			next_delay = (adjusted_start - frames[i][2]) % (24 * 60)
 		end
 	end
 
